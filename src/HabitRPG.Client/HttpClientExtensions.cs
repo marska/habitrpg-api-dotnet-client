678984772fc776d36c61991f5813905dcfa3a8df
negue <eugen.bolz@gmail.com>
2014-06-13 19:54:00 +0200

    ported HabitRPG.Client to portable class library

---
+++ src/HabitRPG.Client/HttpClientExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitRPG.Client
+{
+   public static class HttpClientExtensions
+   {
+      public static Task<HttpResponseMessage> PostAsJsonAsync(this HttpClient client, string url, object obj)
+      {
+         var jsonContent = JsonConvert.SerializeObject(obj);
+
+         var postMessage = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+         return client.PostAsync(url, postMessage);
+      }
+   }
+}
