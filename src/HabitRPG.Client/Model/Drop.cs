059bc342215d9786fbc4bcddea67d594d25730f5
Negue <eugen.bolz@gmail.com>
2014-09-11 22:48:55 +0200

    Added ScoreResult and POST/PUT/DELETE Tags

---
+++ src/HabitRPG.Client/Model/Drop.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace HabitRPG.Client.Model
+{
+   public class Drop
+   {
+      [JsonProperty("text")]
+      public string Text { get; set; }
+
+      [JsonProperty("type")]
+      public string Type { get; set; }
+
+      [JsonProperty("dialog")]
+      public string Dialog { get; set; }
+
+      [JsonProperty("value")]
+      public string Value { get; set; }
+
+      [JsonProperty("key")]
+      public string Key { get; set; }
+
+      [JsonProperty("notes")]
+      public string Notes { get; set; }
+   }
+}
---
+++ src/HabitRPG.Client/Model/ScoreResult.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace HabitRPG.Client.Model
+{
+   public class ScoreResult : Stats
+   {
+      /// <summary>
+      /// Delta of the added Task.Value
+      /// </summary>RandomReward
+      [JsonProperty("delta")]
+      public double Delta { get; set; }
+
+      [JsonProperty("_tmp")]
+      public RandomReward RandomReward { get; set; }
+
+      [JsonProperty("buffs")]
+      public Buffs Buffs { get; set; }
+
+      [JsonProperty("points")]
+      public double Points { get; set; }
+   }
+}
