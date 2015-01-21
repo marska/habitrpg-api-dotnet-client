a0707b24af5b2dbb5822e4404417e7a5071d1dce
Michael Holt <mdholt@xactware.com>
2015-01-20 21:56:11 -0700

    Added ImageUrl and Blurb to the Profile class.
    Those values can be present when you get the User information.

--- src/HabitRPG.Client/Model/Profile.cs
+++ src/HabitRPG.Client/Model/Profile.cs
@@ -5,7 +5,13 @@ namespace HabitRPG.Client.Model
 {
    public class Profile
    {
-      [JsonProperty("name")]
-      public String Name { get; set; }
+		[JsonProperty("name")]
+		public String Name { get; set; }
+
+		[JsonProperty("imageUrl")]
+		public String ImageUrl { get; set; }
+
+		[JsonProperty("blurb")]
+		public String Blurb { get; set; }
    }
 }
