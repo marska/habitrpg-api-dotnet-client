763aaa414302eafa5f4da3012a706071f498eb5e
Michael Holt <CutFlame@gmail.com>
2015-01-28 20:29:12 -0700

    Added operator overloads for StatsBase class.

--- src/HabitRPG.Client/Model/StatsBase.cs
+++ src/HabitRPG.Client/Model/StatsBase.cs
@@ -15,5 +15,65 @@ namespace HabitRPG.Client.Model
 
       [JsonProperty("str")]
       public double Str { get; set; }
+
+		public static StatsBase operator+(StatsBase one, StatsBase two)
+		{
+			var result = new StatsBase();
+			result.Con = one.Con + two.Con;
+			result.Int = one.Int + two.Int;
+			result.Per = one.Per + two.Per;
+			result.Str = one.Str + two.Str;
+			return result;
+		}
+
+		public static StatsBase operator+(StatsBase one, int val)
+		{
+			var result = new StatsBase();
+			result.Con = one.Con + val;
+			result.Int = one.Int + val;
+			result.Per = one.Per + val;
+			result.Str = one.Str + val;
+			return result;
+		}
+
+		public static StatsBase operator+(StatsBase one, double val)
+		{
+			var result = new StatsBase();
+			result.Con = one.Con + val;
+			result.Int = one.Int + val;
+			result.Per = one.Per + val;
+			result.Str = one.Str + val;
+			return result;
+		}
+
+		public static StatsBase operator-(StatsBase one, StatsBase two)
+		{
+			var result = new StatsBase();
+			result.Con = one.Con - two.Con;
+			result.Int = one.Int - two.Int;
+			result.Per = one.Per - two.Per;
+			result.Str = one.Str - two.Str;
+			return result;
+		}
+
+		public static StatsBase operator-(StatsBase one, int val)
+		{
+			var result = new StatsBase();
+			result.Con = one.Con - val;
+			result.Int = one.Int - val;
+			result.Per = one.Per - val;
+			result.Str = one.Str - val;
+			return result;
+		}
+
+		public static StatsBase operator/(StatsBase one, int val)
+		{
+			var result = new StatsBase();
+			result.Con = one.Con / val;
+			result.Int = one.Int / val;
+			result.Per = one.Per / val;
+			result.Str = one.Str / val;
+			return result;
+		}
    }
 }
